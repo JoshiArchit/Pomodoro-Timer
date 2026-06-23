551ce0bb0e04b54a2bdc654fd8a4e92318454323
Archit Satish Joshi <joshi.archit.2904@gmail.com>
2026-06-22 19:57:54 -0500

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Archit Joshi on 6/22/26.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
