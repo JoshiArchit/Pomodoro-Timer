cc1c024e8710ee8092acc54afa6af23d4d2e8d33
Archit Satish Joshi <joshi.archit.2904@gmail.com>
2026-06-22 20:14:28 -0500

    Repository setup and watchOS target
    add gitignore

---
+++ Pomodoro Watch App Watch App/Pomodoro_Watch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_Watch_AppApp.swift
+//  Pomodoro Watch App Watch App
+//
+//  Created by Archit Joshi on 6/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_Watch_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
