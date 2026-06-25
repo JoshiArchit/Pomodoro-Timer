307e9046fb0e71d035fb349a3a104fac1fe4a811
Archit Satish Joshi <joshi.archit.2904@gmail.com>
2026-06-25 18:05:54 -0500

    feat: add iOS app scaffolding with placeholder views

---
+++ PomodoroTimer/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  PomodoroTimer
+//
+//  Created by Archit Joshi on 6/25/26.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text("Timer")
+    }
+}
+
+#Preview {
+    TimerView()
+}
